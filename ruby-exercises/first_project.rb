d8aecb7cf440f169df8f65e9cfc8c4da5066ee2e
Eduardo Sanchez <eddiekidd98@gmail.com>
2023-03-07 09:11:27 -0600

    second day learning monday of march 7 20023

--- ruby-exercises/first_project.rb
+++ ruby-exercises/first_project.rb
@@ -1,11 +1,52 @@
-status = true
-while status
-    puts 'Enter you answer or enter "n" to end'
-    answer = gets.chomp
-    if answer == 'n'
+users = [
+    { username: "eduardo", password: "password1"},
+    { username: "fernando", password: "password2"},
+    { username: "susan", password: "password3"},
+    { username: "miriam", password: "password4"}
+]
+
+
+
+runing = true
+counter = 0
+loggin_status = nil
+credential = {}
+while runing 
+    puts "enter you name"
+    name = gets.chomp
+    puts "enter you password or 'n' to close"
+    password = gets.chomp
+    
+    users.each do | v |
+        if v[:username] == name && v[:password] == password
+            credential[:username] = v[:username]
+            credential[:password] = v[:password]
+            loggin_status = true
+        end
+    end
+
+    if password == 'n'
         puts "See you latter"
-        status = false
+        runing = false
+    elsif loggin_status
+        puts "Welcome"
+        puts credential
+        puts "Whants to continue? y/n"
+        answer = gets.chomp
+        if answer == 'n'
+            runing = false
+        else
+            counter = 0
+            loggin_status = false
+            puts "loop keeping working"
+            puts "counter #{counter}"
+        end
     else
+        counter += 1
+        if counter == 3
+            runing = false
+        end
         puts "loop keeping working"
+        puts "counter #{counter}"
     end
 end
---
+++ ruby-exercises/styling_in_ruby.rb
@@ -0,0 +1,10 @@
+#for default we are going to use snake_case for example variables and methods(function) perro_loco
+#remember the indentation and remember that in ruby and rails 2 spaces for indentation
+#class names remember that every object is a class and in general camel case is used PerroLoco
+def new_method
+
+end
+
+class NewMethod 
+
+end
